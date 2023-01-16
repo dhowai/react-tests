525f76fbb22472149d68a473453ad055b5d51999
DESKTOP-2I0AT47\demon <dhowai.dev@gmail.com>
2023-01-16 15:34:16 +0000

    Test user interaction and state

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,14 +1,55 @@
-import { render, screen } from "@testing-library/react";
+import { getByRole, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
-test("renders hello world as text", () => {
-  // Arange
-  render(<Greeting />);
+describe("Greeting component", () => {
+  test("renders hello world as text", () => {
+    // Arange
+    render(<Greeting />);
 
-  //Act
-  // null
+    //Act
+    // null
 
-  // Assert
-  const helloWorldElement = screen.getByText("Hello World");
-  expect(helloWorldElement).toBeInTheDocument();
+    // Assert
+    const helloWorldElement = screen.getByText("Hello World");
+    expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders Default if button was not clicked", () => {
+    // Arange
+    render(<Greeting />);
+
+    // Act
+    // null
+
+    // Assert
+    const outputElement = screen.getByText("Default", { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("renders Changed! if the button was clicked", () => {
+    // Arange
+    render(<Greeting />);
+
+    //Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.getByText("Changed!");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("Does not render Default if the button was clicked", () => {
+    // Arange
+    render(<Greeting />);
+
+    //Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.queryByText("Default", { exact: false });
+    expect(outputElement).toBeNull();
+  });
 });
